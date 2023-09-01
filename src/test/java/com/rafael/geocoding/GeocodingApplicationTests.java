cefd602e48373b21233edd7c4af3fa66099a62a0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-01 11:10:13 -0300

    Commit inicial

---
+++ src/test/java/com/rafael/geocoding/GeocodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.geocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

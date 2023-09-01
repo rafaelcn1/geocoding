1588b72bb1c696f6eed2bc65a1415a4561341527
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-01 19:05:04 -0300

    fix: implementação do Swagger no main

--- src/main/java/com/rafael/geocoding/GeocodingApplication.java
+++ src/main/java/com/rafael/geocoding/GeocodingApplication.java
@@ -3,7 +3,10 @@ package com.rafael.geocoding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class GeocodingApplication {
 
 	public static void main(String[] args) {
